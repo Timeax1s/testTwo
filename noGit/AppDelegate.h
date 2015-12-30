8657ebd54573b675445643d278b85f28eb263805
dc008 <dc008@cuixiaoyu.lan>
2015-12-30 14:05:21 +0800

    first commit

---
+++ noGit/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  noGit
+//
+//  Created by dc008 on 15/12/30.
+//  Copyright © 2015年 lin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ noGit/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  noGit
+//
+//  Created by dc008 on 15/12/30.
+//  Copyright © 2015年 lin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
